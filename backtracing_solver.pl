637403b830e16a8f586c0277e7324a1d3c671fbb
Caleb Blondell <caleb.blondell@gmail.com>
2025-12-02 05:27:52 -0800

    constrain to 1-4

--- backtracing_solver.pl
+++ backtracing_solver.pl
@@ -2,10 +2,18 @@
 /** Sol = [4,1,2,3,2,3,4,1,1,2,3,4,3,4,1,2] */
 
 value_or_null(X, V) :-
-    var(X); X == V.
+    (var(X); X == V).
 
 sudoku([I0,I1,I2,I3,I4,I5,I6,I7,I8,I9,Ia,Ib,Ic,Id,Ie,If],
        [S0,S1,S2,S3,S4,S5,S6,S7,S8,S9,Sa,Sb,Sc,Sd,Se,Sf]) :-
+
+    % All solution cells must be between 1 and 4
+    between(1,4,S0), between(1,4,S1), between(1,4,S2), between(1,4,S3),
+    between(1,4,S4), between(1,4,S5), between(1,4,S6), between(1,4,S7),
+    between(1,4,S8), between(1,4,S9), between(1,4,Sa), between(1,4,Sb),
+    between(1,4,Sc), between(1,4,Sd), between(1,4,Se), between(1,4,Sf),
+
+    % All solution cells must be equal to the input cell, or not yet assigned
     value_or_null(I0, S0),
     value_or_null(I1, S1),
     value_or_null(I2, S2),
