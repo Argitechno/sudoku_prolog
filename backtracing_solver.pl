8d118e551878ff894e379b40523442de975c4153
Caleb Blondell <caleb.blondell@gmail.com>
2025-12-02 05:30:05 -0800

    remove redundant null check

--- backtracing_solver.pl
+++ backtracing_solver.pl
@@ -1,8 +1,8 @@
 /** Sudoku([_,_,2,3,_,_,_,_,_,_,_,_,3,4,_,_], Sol). */
 /** Sol = [4,1,2,3,2,3,4,1,1,2,3,4,3,4,1,2] */
 
-value_or_null(X, V) :-
-    (var(X); X == V).
+value(X, V) :-
+    (X = V).
 
 sudoku([I0,I1,I2,I3,I4,I5,I6,I7,I8,I9,Ia,Ib,Ic,Id,Ie,If],
        [S0,S1,S2,S3,S4,S5,S6,S7,S8,S9,Sa,Sb,Sc,Sd,Se,Sf]) :-
@@ -14,22 +14,19 @@ sudoku([I0,I1,I2,I3,I4,I5,I6,I7,I8,I9,Ia,Ib,Ic,Id,Ie,If],
     between(1,4,Sc), between(1,4,Sd), between(1,4,Se), between(1,4,Sf),
 
     % All solution cells must be equal to the input cell, or not yet assigned
-    value_or_null(I0, S0),
-    value_or_null(I1, S1),
-    value_or_null(I2, S2),
-    value_or_null(I3, S3),
-    value_or_null(I4, S4),
-    value_or_null(I5, S5),
-    value_or_null(I6, S6),
-    value_or_null(I7, S7),
-    value_or_null(I8, S8),
-    value_or_null(I9, S9),
-    value_or_null(Ia, Sa),
-    value_or_null(Ib, Sb),
-    value_or_null(Ic, Sc),
-    value_or_null(Id, Sd),
-    value_or_null(Ie, Se),
-    value_or_null(If, Sf).
-
-    
-
+    value(I0, S0),
+    value(I1, S1),
+    value(I2, S2),
+    value(I3, S3),
+    value(I4, S4),
+    value(I5, S5),
+    value(I6, S6),
+    value(I7, S7),
+    value(I8, S8),
+    value(I9, S9),
+    value(Ia, Sa),
+    value(Ib, Sb),
+    value(Ic, Sc),
+    value(Id, Sd),
+    value(Ie, Se),
+    value(If, Sf).
